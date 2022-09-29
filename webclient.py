87a781ffafde78bac629ce6198a989f3d1874292
Caleb Knight <knightca@oregonstate.edu>
2022-09-29 12:04:11 -0700

    Updated a few comments

--- webclient.py
+++ webclient.py
@@ -1,19 +1,23 @@
 import socket
 import sys
 
+
+# look for user input for a host and a port
 if len(sys.argv) >= 3:
 	host = sys.argv[1]
 	port = int(sys.argv[2])
 else:
-	host = "www.example.com"
-	port = 80
+	host = "www.example.com"   # if no UI, use this host
+	port = 80                  # and this port
 
+# connections
 s = socket.socket()
 s.connect((host, port))
 
 get = "GET / HTTP/1.1 200 OK\r\nHost: {}\r\nConnection: close\r\n\r\n".format(host)
 s.sendall(get.encode("ISO-8859-1"))
 
+# as long as we are recv data, print and decode the response
 while True:
 	resp = s.recv(4096)
 	if not resp: break
